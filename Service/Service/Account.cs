951458722f7855fd997671ac217661442e43b250
kamranss <kamranss@code.edu.az>
2022-12-18 19:27:05 +0400

    Feature: added new Project -helpers-, -exception-

---
+++ Service/Service/Account.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Models;
+using Service.Helpers;
+using Service.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    public class Account : IAccount
+    {
+        public void Register(string username, string email, string password)
+        {
+			try
+			{
+				User user1 = new()
+				{
+					Username = username,
+					Email = email,
+					Password = password
+				};
+
+				if (user1.Email.Contains("@"))
+				{
+					Console.WriteLine("Register Succesed");
+				}	
+				
+				
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(UserCredentialException.UserEmailWrong);
+				
+			}
+        }
+    }
+}
